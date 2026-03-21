0ce8b78b69311356347cc3f5f8a0499975b2d332
ezekieltemitope <alebiosutemitope79@gmail.com>
2026-03-21 04:14:12 +0100

    footer updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Instagram, Twitter, Facebook, Linkedin } from "lucide-react";
+import { Twitter, Facebook, Linkedin, Music2 } from "lucide-react";
 
 type FooterProps = {
   onOpenTerms: () => void;
@@ -9,6 +9,7 @@ export default function Footer({ onOpenTerms, onOpenFaq }: FooterProps) {
   return (
     <footer className="bg-[#01172C] text-white py-12 px-6 mt-12">
       <div className="max-w-7xl mx-auto text-center space-y-6">
+        {/* LINKS */}
         <div className="flex justify-center gap-6 text-sm">
           <button
             type="button"
@@ -35,13 +36,42 @@ export default function Footer({ onOpenTerms, onOpenFaq }: FooterProps) {
           </button>
         </div>
 
+        {/* SOCIAL ICONS */}
         <div className="flex justify-center gap-6">
-          <Instagram className="w-5 h-5 hover:opacity-80 cursor-pointer" />
-          <Twitter className="w-5 h-5 hover:opacity-80 cursor-pointer" />
-          <Facebook className="w-5 h-5 hover:opacity-80 cursor-pointer" />
-          <Linkedin className="w-5 h-5 hover:opacity-80 cursor-pointer" />
+          <a
+            href="https://x.com/loadrivo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Twitter className="w-5 h-5 hover:opacity-80 cursor-pointer" />
+          </a>
+
+          <a
+            href="https://www.facebook.com/share/17NKtVFr8z/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Facebook className="w-5 h-5 hover:opacity-80 cursor-pointer" />
+          </a>
+
+          <a
+            href="https://www.linkedin.com/company/loadrivo/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Linkedin className="w-5 h-5 hover:opacity-80 cursor-pointer" />
+          </a>
+
+          <a
+            href="https://www.tiktok.com/@loadrivo4?_r=1&_t=ZS-94REMkzcFhE"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Music2 className="w-5 h-5 hover:opacity-80 cursor-pointer" />
+          </a>
         </div>
 
+        {/* COPYRIGHT */}
         <p className="text-xs text-white/70">
           © 2022 Loadrivo. All right reserved
         </p>
